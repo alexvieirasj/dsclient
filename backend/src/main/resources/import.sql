56575626f54ba4fa538310e2406d0f48ed6508b1
alexvieirasj <alexvieiracb20@gmail.com>
2022-08-20 00:09:43 -0300

    CRUD complete

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Fabiana Emanuelly', '223.590.120-45', 1850 ,TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_Date, children) VALUES ('Victor Carlos Galvão', '853.734.520-25', 2150 ,TIMESTAMP WITH TIME ZONE '2021-02-12T16:50:07.12345Z', 3);
